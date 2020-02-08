63a32109c655794b4b42ccd17b20e094d4a768aa
cycz <838143144@qq.com>
2020-02-08 16:15:47 +0800

    bug

--- message.py
+++ message.py
@@ -15,7 +15,7 @@ class message(object):
             if not mail:
                 raise Exception('mail can not be empty')
             self.mail = mail
-        self.messageTtpe = messageType
+        self.messageType = messageType
 
     def send(self, desp='', isOrder=False):
         desp = str(desp)
@@ -23,7 +23,7 @@ class message(object):
             msg = desp + ' 类型口罩，已经下单了。24小时内付款'
         else:
             msg = desp + ' 类型口罩，下单失败了，快去抢购！'
-        if self.messageTtpe == '1':
+        if self.messageType == '1':
             sendMail(self.mail, msg)
-        if self.messageTtpe == '2':
+        if self.messageType == '2':
             sendWechat(sc_key=self.sc_key, desp=msg)
